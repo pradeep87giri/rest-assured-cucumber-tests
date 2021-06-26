27249070013c9154264aa837df83d89ee51679d8
giripra <pradeep.giri@adidas.com>
2021-06-26 23:33:00 +0530

    commenting sending email in Jenkins file

--- resources/jenkinsFiles/jenkinsFile.groovy
+++ resources/jenkinsFiles/jenkinsFile.groovy
@@ -4,10 +4,6 @@
 def gitRepo = 'https://github.com/pradeep87giri/petStore.git'
 def gitBranch = 'master'
 
-// Pipeline config
-def BUILD_STATUS
-def emailID = 'pradeep87giri@gmail.com'
-
 
 //Pipeline
 pipeline {
@@ -15,7 +11,7 @@ pipeline {
     stages {
         stage('Get the Git repo') {
             steps {
-                echo 'Downloading the test automation code'
+                echo 'Downloading the tes   t automation code'
                 git branch: gitBranch, url: gitRepo
             }
         }
@@ -27,20 +23,20 @@ pipeline {
             }
         }
 
-        stage("Emailing the result") {
+        //Below step can be used to email the result
+/*        stage("Emailing the result") {
             steps {
-                BUILD_STATUS = currentBuild.currentResult
                 echo 'Emailing report '
                 emailext body: "Kindly find below the results of the automated tests executed. <BR>" +
                         "<BR><ul>Job: <strong>${JOB_NAME}</strong></ul>" +
                         "<ul>Build: <strong>${BUILD_NUMBER}</strong></ul>" +
-                        "<ul>Status: <strong>${BUILD_STATUS}</strong></ul><BR>" +
+                        "<ul>Status: <strong>" + currentBuild.currentResult + "</strong></ul><BR>" +
                         "<p>Check <strong>console</strong> output at ${BUILD_URL}console.<BR></p>" +
                         "Best regards,<BR><BR> Pradeep Giri",
-                        subject: "${JOB_NAME} - Build # ${BUILD_NUMBER} - ${BUILD_STATUS}!",
-                        recipientProviders: emailID,
+                        subject: "${JOB_NAME} - Build # ${BUILD_NUMBER} - " + currentBuild.currentResult + "!",
+                        recipientProviders: 'pradeep1987giri@gmail.com',
                         mimeType: "text/html"
             }
-        }
+        }*/
     }
 }
